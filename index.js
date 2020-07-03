f1954f4df336f98980ceb1db3155c69b253c13c1
Kelly Alves <kellyp.alves87@gmail.com>
2020-07-03 14:05:14 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+fs = require("fs");
+
+console.log(process.argv);
+
+const md = process.argv.slice(2);
+
+console.log(md);
+
+fs.readFile(md[0], "utf8", function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
